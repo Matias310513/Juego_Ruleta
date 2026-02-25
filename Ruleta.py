dadeb9e115daf1b35a4805014ab73deb76a039b0
Matias <matiasgomezmaya@gmail.com>
2026-02-25 17:54:40 -0500

    Se agrega un sistema para mostrar cuantos segundos tardaste en ganar

--- Ruleta.py
+++ Ruleta.py
@@ -7,6 +7,7 @@ lista3 = ['Manzana','Cereza','Uva','Limon','7']
 lista1choice =  random.choice(lista1)
 lista2choice =  random.choice(lista2)
 lista3choice =  random.choice(lista3)
+inicio = time.time()
 
 while lista1choice != lista2choice or lista1choice != lista3choice or lista2choice != lista3choice:
     print(lista1choice,lista2choice,lista3choice)
@@ -14,7 +15,9 @@ while lista1choice != lista2choice or lista1choice != lista3choice or lista2choi
     lista1choice =  random.choice(lista1)
     lista2choice =  random.choice(lista2)
     lista3choice =  random.choice(lista3)
-    time.sleep(1)
+    time.sleep(0.5)
 
+fin = time.time()
+diferencia = int(fin - inicio)
 print(lista1choice,lista2choice,lista3choice)
-print('Ganaste')
+print(f'Ganaste, te demoraste {diferencia} segundos en ganar')
