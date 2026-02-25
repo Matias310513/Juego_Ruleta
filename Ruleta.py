1494acdceb7782a0367033f9ccf512a2aef9fa48
Matias <matiasgomezmaya@gmail.com>
2026-02-25 18:23:50 -0500

    Se agrega un sistema para que si se tarda mas de 60 segundos en terminar se cambie de segundos a minutos y un contador para saber cuantos intentos demoraste en ganar

--- Ruleta.py
+++ Ruleta.py
@@ -4,10 +4,10 @@ lista1 = ['Manzana','Cereza','Uva','Limon','7']
 lista2 = ['Manzana','Cereza','Uva','Limon','7']
 lista3 = ['Manzana','Cereza','Uva','Limon','7']
 
+inicio = time.time()
 lista1choice =  random.choice(lista1)
 lista2choice =  random.choice(lista2)
 lista3choice =  random.choice(lista3)
-inicio = time.time()
 
 while lista1choice != lista2choice or lista1choice != lista3choice or lista2choice != lista3choice:
     print(lista1choice,lista2choice,lista3choice)
@@ -15,9 +15,16 @@ while lista1choice != lista2choice or lista1choice != lista3choice or lista2choi
     lista1choice =  random.choice(lista1)
     lista2choice =  random.choice(lista2)
     lista3choice =  random.choice(lista3)
+    intentos = intentos+1
     time.sleep(0.5)
 
 fin = time.time()
 diferencia = int(fin - inicio)
 print(lista1choice,lista2choice,lista3choice)
-print(f'Ganaste, te demoraste {diferencia} segundos en ganar')
+if diferencia > 60:
+    minutos = diferencia // 60
+    segundos = diferencia - (minutos*60)
+    print(f'Ganaste, te demoraste {minutos} minutos y {segundos} segundos y {intentos} en ganar')
+
+else:
+    print(f'Ganaste, te demoraste {diferencia} segundos y {intentos} en ganar')
