a7e681e24d6d1f3fba9f413589d486b4767cf9e2
Matias <matiasgomezmaya@gmail.com>
2026-02-25 17:27:14 -0500

    Se agrega condiciones y comparaciones para ganar o perder

--- Ruleta.py
+++ Ruleta.py
@@ -7,4 +7,9 @@ lista3 = ['Manzana','Cereza','Uva','Limon','7']
 lista1choice =  random.choice(lista1)
 lista2choice =  random.choice(lista2)
 lista3choice =  random.choice(lista3)
+print(lista1choice,lista2choice,lista3choice)
+if lista1choice == lista2choice == lista3choice:
+    print('Ganste!!!')
 
+else:
+    print('Perdiste')
