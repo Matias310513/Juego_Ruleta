5670c6353bba2bccccf43907035d58376fa2d58e
Matias <matiasgomezmaya@gmail.com>
2026-02-25 18:32:43 -0500

    Arreglo de funcion intentos

--- Ruleta.py
+++ Ruleta.py
@@ -8,6 +8,7 @@ inicio = time.time()
 lista1choice =  random.choice(lista1)
 lista2choice =  random.choice(lista2)
 lista3choice =  random.choice(lista3)
+intentos = 0
 
 while lista1choice != lista2choice or lista1choice != lista3choice or lista2choice != lista3choice:
     print(lista1choice,lista2choice,lista3choice)
@@ -24,7 +25,7 @@ print(lista1choice,lista2choice,lista3choice)
 if diferencia > 60:
     minutos = diferencia // 60
     segundos = diferencia - (minutos*60)
-    print(f'Ganaste, te demoraste {minutos} minutos y {segundos} segundos y {intentos} en ganar')
+    print(f'Ganaste, te demoraste {minutos} minutos y {segundos} segundos y {intentos} intentos en ganar')
 
 else:
-    print(f'Ganaste, te demoraste {diferencia} segundos y {intentos} en ganar')
+    print(f'Ganaste, te demoraste {diferencia} segundos y {intentos} intentos en ganar')
