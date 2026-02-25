884d917f044b0e280b78d50e78caf1899eaa1c62
Matias <matiasgomezmaya@gmail.com>
2026-02-25 17:43:41 -0500

    Se agrega un sistema automatico donde hasta que no ganes siga intentando

--- Ruleta.py
+++ Ruleta.py
@@ -1,4 +1,4 @@
-import random
+import random, time
 
 lista1 = ['Manzana','Cereza','Uva','Limon','7']
 lista2 = ['Manzana','Cereza','Uva','Limon','7']
@@ -7,9 +7,14 @@ lista3 = ['Manzana','Cereza','Uva','Limon','7']
 lista1choice =  random.choice(lista1)
 lista2choice =  random.choice(lista2)
 lista3choice =  random.choice(lista3)
-print(lista1choice,lista2choice,lista3choice)
-if lista1choice == lista2choice == lista3choice:
-    print('Ganste!!!')
 
-else:
-    print('Perdiste')
+while lista1choice != lista2choice or lista1choice != lista3choice or lista2choice != lista3choice:
+    print(lista1choice,lista2choice,lista3choice)
+    print('Perdiste\n')
+    lista1choice =  random.choice(lista1)
+    lista2choice =  random.choice(lista2)
+    lista3choice =  random.choice(lista3)
+    time.sleep(1)
+
+print(lista1choice,lista2choice,lista3choice)
+print('Ganaste')
